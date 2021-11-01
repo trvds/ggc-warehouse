c4ba4f4b46ed8e5b3a66ac8dd9bc28c547c55f1c
trvds <tiagovs02@gmail.com>
2021-11-01 20:07:45 +0000

    Added recipe to BreakdownTransaction

--- ggc-core/src/ggc/BreakdownTransaction.java
+++ ggc-core/src/ggc/BreakdownTransaction.java
@@ -3,14 +3,16 @@ package ggc;
 public class BreakdownTransaction extends Transaction{
     private float _paymentPrice;
     private int _dateDeadline;
+    private String _recipe;
 
-    public BreakdownTransaction(int id, int paymentDate, String productId, String partnerId, int quantity, int price){
+    public BreakdownTransaction(int id, int paymentDate, String productId, String partnerId, int quantity, int price, String recipe){       
         super(id, paymentDate, productId, partnerId, quantity, price);
+        _recipe = recipe; 
     }
 
     @Override
     public String toString(){
-        return "DESAGREGAÇÃO|" + super.toString() + "|" + _paymentPrice + "|" + _dateDeadline + "|" + getPaymentDate();
+        return "DESAGREGAÇÃO|" + super.toString() + "|" + _paymentPrice + "|" + _dateDeadline + "|" + getPaymentDate() + "|" + _recipe;
     }
 
 }
